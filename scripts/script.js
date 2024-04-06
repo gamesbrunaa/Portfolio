15b489596fa6ffd4f1cf8ec42db589819d7d2c6f
Bruna Veloso <91560228+gamesbrunaa@users.noreply.github.com>
2024-04-05 21:25:28 -0300

    create nav

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+const bto1 = document.getElementById('agni');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');
+})
+
+function trocar(element) {
+    if(element.id == "agni") {
+        element.id = "agni2";
+    } else { //This will only execute when elemsent.id == "secondId"
+        element.id = "agni";
+    }
+}
+
+bto1.addEventListener('click', () => {
+    document.getElementById("agni").src="/assets/icon2/sun 1.png";
+});
+
+bto1.addEventListener('click', () => {
+    document.getElementById("agni2").src="/assets/icon2/moon 1.png";
+});
